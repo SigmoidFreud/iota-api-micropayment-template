7dbb86efba4fa830025a6350b55d005c4331d006
Sushanth Kodali <kodalisushanth@gmail.com>
2017-04-01 14:50:00 -0400

    Update IOTATransferRestRequestTemplate.py
    update script to assign uuid as tag to identify requests and only verify requests with matching request-id's

--- IOTATransferRestRequestTemplate.py
+++ IOTATransferRestRequestTemplate.py
@@ -4,10 +4,21 @@ from __future__ import absolute_import, division, print_function, \
 import requests
 from iota import *
 from iota.adapter.sandbox import SandboxAdapter
+import uuid
 from pprint import pprint
 
-# sample dummy api request
-sample_request = requests.get("https://api.github.com/user", auth=('user', 'passwd'))
+
+# this method will register the request by generating a unique id for the request, this will serve as the tag for the
+# sample transaction
+def register_request():
+    return uuid.uuid5()
+
+
+# sample dummy api request, will return dictionary of request object and UUID of the requst
+
+def create_request(url="https://api.github.com/users/sigmoidfreud/repos", headers={}):
+    return {"request": requests.get(url, headers), "request-id": uuid.uuid5()}
+
 
 # Create the API object.
 iota = Iota(
@@ -26,43 +37,49 @@ iota = Iota(
     seed=b'SEED9GOES9HERE',
 )
 
+
 # Example of sending a transfer using the sandbox.
 # For more information, see :py:meth:`Iota.send_transfer`.
 # noinspection SpellCheckingInspection
-default_depth = 100
+def create_transaction_dictionary(depth=100, request_tag=None,
+                                  payment_address=b'TESTVALUE9DONTUSEINPRODUCTION99999FBFFTG'):
+    sample_transaction = ProposedTransaction(
+        # API payment address.
+        address=
+        Address(
+            bytes(payment_address)
+        ),
 
-sample_transaction = ProposedTransaction(
-    # API payment address.
-    address=
-    Address(
-        b'TESTVALUE9DONTUSEINPRODUCTION99999FBFFTG'
-        b'QFWEHEL9KCAFXBJBXGE9HID9XCOHFIDABHDG9AHDR'
-    ),
+        # Amount of IOTA to transfer.
+        # This value may be zero.
+        value=17,
 
-    # Amount of IOTA to transfer.
-    # This value may be zero.
-    value=17,
+        # Optional tag to attach to the transfer.
+        tag=Tag(bytes(request_tag)),
 
-    # Optional tag to attach to the transfer.
-    tag=Tag(b'EXAMPLE'),
-
-    # Optional message to include with the transfer.
-    message=TryteString.from_string('I am making an API Request!'),
-)
+        # Optional message to include with the transfer.
+        message=TryteString.from_string('I am making an API Request!'),
+    )
+    return {"depth": depth, "transaction-object": sample_transaction, "tag": request_tag}
 
 
-def requestData(api_request=sample_request):
+def requestData():
+    request_id = register_request()
+    transaction = create_transaction_dictionary(request_tag=id)
     iota.send_transfer(
-        depth=default_depth,
+        depth=transaction['depth'],
 
         # One or more :py:class:`ProposedTransaction` objects to add to the
         # bundle.
-        transfers=[sample_transaction],
+        transfers=[transaction],
     )
-    if sample_transaction.value == 17:
-        return api_request.json()
+    request_dict = create_request(headers={"auth_token": transaction.branch_transaction_hash})
+    if transaction['tag'] == request_id:
+        return request_dict['request'].json()
+
 
 def main():
     requestData()
+
 
 if __name__ == "__main__": main()
